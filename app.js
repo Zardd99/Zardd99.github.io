0b048287858a9ecbfd0eb48afb03a23071e7f6a0
Sakda Chin <127505436+Zardd99@users.noreply.github.com>
2024-02-27 00:23:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 function alertButton() {
-  alert("Love You (:")
+  var newWindow = window.open("", "new window", "width=200,height=100");
+  newWindow.document.write("<h1>I love gaming (:</h1>");
 }
