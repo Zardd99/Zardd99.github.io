2adaec08ecdf2eaf6363e9f91cab76bc07b77224
Sakda Chin <127505436+Zardd99@users.noreply.github.com>
2024-06-05 08:18:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 function alertButton() {
   var myWindow = window.open("", "MsgWindow", "fullscreen=yes");
-  myWindow.document.write("<p class='custom-font'>I love gaming also (:</p>");
+  myWindow.document.write("<p class='custom-font'>I love anime also (:</p>");
 }
